23184a638b57e46f1b2526068d3363df26ace5c3
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-22 15:52:50 -0700

    Create Coding Activity 2.java
    Write a program that asks the user for three names, then prints the names in reverse order.

---
+++ Unit 01/Lesson 2/Coding Activity 2.java
@@ -0,0 +1,20 @@
+/* Lesson 2 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+class U1_L2_Activity_Two {
+  public static void main(String[] args) {
+    
+    /* Write your code here */
+    Scanner scan = new Scanner(System.in);
+    String a;
+    String b;
+    String c;
+    
+    System.out.println("Please enter three names:");
+    a = scan.nextLine();
+    b = scan.nextLine();
+    c = scan.nextLine();
+    System.out.println(c + " " + b + " " + a);
+  }
+}
