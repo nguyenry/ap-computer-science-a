ae83ccb0a493e996c5ee6905c5ab7b4b628cb25f
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-22 15:56:30 -0700

    Create Coding Activity 2.java
    Write a program which takes user input of a number of inches, and then prints the number of whole feet in that many inches (remember there are 12 inches in a foot).
    
    For example if the user enters
    56
    
    The program should print
    4
    
    As there are 4 whole feet in 56 inches (56'' is the same as 4'8'')

---
+++ Unit 01/Lesson 4/Coding Activity 2.java
@@ -0,0 +1,16 @@
+/* Lesson 4 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+class U1_L4_Activity_Two {
+    public static void main(String[] args) {
+      
+       /* Write your code here */
+      Scanner scan = new Scanner(System.in);
+      int inch;
+      
+      System.out.println("Input pls");
+      inch = scan.nextInt();
+      System.out.println(inch / 12);
+    }
+}
