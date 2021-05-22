3f130969d1ac5bc4dc46dd3afa470ffd17ce6c0f
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-22 15:54:09 -0700

    Create Coding Activity 1.java
    Write a program that prompts a user to input two doubles and then prints these two doubles backward on the screen.
    
    So if the user enters:
    1.3   6.8
    
    The program should output:
    6.8 1.3
    
    Notice there is exactly one space between the numbers.

---
+++ Unit 01/Lesson 3/Coding Activity 1.java
@@ -0,0 +1,18 @@
+/* Lesson 3 Coding Activity Question 1 */
+
+import java.util.Scanner;
+
+class U1_L3_Activity_One {
+  public static void main(String[] args) {
+      
+    /* Write your code here */
+    Scanner scan = new Scanner(System.in);
+    double n;
+    double m;
+    
+    System.out.println("Input pls");
+    n = scan.nextDouble();
+    m = scan.nextDouble();
+    System.out.println(m + " " + n);
+  }
+}
