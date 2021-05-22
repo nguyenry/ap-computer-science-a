d96f1fb65499199b2db697983fca9aaf12992cc4
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-22 16:10:56 -0700

    Create Coding Activity 1.java
    Write code which creates three regular polygons with 11, 14 and 19 sides respectively. All side lengths should be 1.0. The code should then print the three shapes, one on each line, in the order given (i.e. the one with 11 sides first and the one with 19 sides last).
    
    Sample run:
    regular hendecagon with side length 1.0
    regular tetrakaidecagon with side length 1.0
    regular enneadecagon with side length 1.0

---
+++ Unit 02/Lesson 5/Coding Activity 1.java
@@ -0,0 +1,18 @@
+/* Lesson 5 Coding Activity Question 1 */
+
+import java.util.Scanner;
+import edhesive.shapes.*;
+
+public class U2_L5_Activity_One{
+  public static void main(String[] args){
+  
+    /* Write your code here */
+    RegularPolygon rp1 = new RegularPolygon(11);
+    RegularPolygon rp2 = new RegularPolygon(14);
+    RegularPolygon rp3 = new RegularPolygon(19);
+    
+    System.out.println(rp1);
+    System.out.println(rp2);
+    System.out.println(rp3);
+  }
+}
