14c77bb9cffb330c50200137d5473e55f48407fc
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-23 13:30:27 -0700

    Create Coding Activity 2.java
    Use a for loop to print the even numbers between 1 and 50, including the first even number, and the number 50. Print each number on a new line.

---
+++ Unit 04/Lesson 3/Coding Activity 2.java
@@ -0,0 +1,15 @@
+/* Lesson 3 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+public class U4_L3_Activity_Two{
+  public static void main(String[] args){
+    
+    /* Write your code here */
+    for (int i = 1; i <= 50; i++) {
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+  }
+}
