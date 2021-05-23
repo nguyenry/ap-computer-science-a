47bf4baee09c55e74a97864105f3cc385156101b
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-23 13:39:37 -0700

    Create Coding Activity 3.java
    Write a void method named updateRadius which takes a circle and an int and changes the value of the circle's radius to the int.
    
    You can call your method in the program's main method so you can test whether it works, but you must remove or comment out the main method before checking your code for a score.

---
+++ Unit 05/Lesson 3/Coding Activity 3.java
@@ -0,0 +1,19 @@
+/* Lesson 3 Coding Activity Question 3 */
+
+import java.util.Scanner;
+import edhesive.shapes.*;
+
+public class U5_L3_Activity_Three{
+
+  /* Add the method updateRadius here */
+  public static void updateRadius(Circle c, int r) {
+    c.setRadius(r);
+  }
+  // You can uncomment and add to the main method to test your code
+  // You will need to remove/comment out this method before checking your code for a score
+  /*
+  public static void main(String[] args){
+    
+  }
+  */
+}
