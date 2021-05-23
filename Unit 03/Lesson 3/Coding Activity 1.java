a10e900652df949e30c1b1f99db79ae7b3f426eb
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-23 12:51:15 -0700

    Create Coding Activity 1.java
    Write a program that takes the input of an integer number from the keyboard and prints "Positive" if it's positive or zero, and print "Negative" otherwise. You must use an else statement to gain full credit for this exercise.
    
    Sample run 1
    Please enter an integer
    7
    Positive
    
    Sample run 2
    Please enter an integer
    -3
    Negative

---
+++ Unit 03/Lesson 3/Coding Activity 1.java
@@ -0,0 +1,22 @@
+/* Lesson 3 Coding Activity Question 1 */
+
+import java.util.Scanner;
+
+public class U3_L3_Activity_One{
+  public static void main(String[] args){
+    
+    /* Write your code here */
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Please enter an integer");
+    int x = scan.nextInt();
+    if (x >= 0)
+    {
+      System.out.println("Positive");
+    }
+    else
+    {
+      System.out.println("Negative");
+    }
+  }
+}
