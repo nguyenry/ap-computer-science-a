da2418dd8c09e85e6581f38623a642bf490f02f0
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-23 12:23:51 -0700

    Create Coding Activity 1.java
    Write a program to test if an integer input from the keyboard is equal to the integer 176. If it is, print "YES" (without the quotes).
    
    Sample run 1:
    Please enter an integer:
    122
    
    Sample run 2:
    Please enter an integer:
    176
    YES

---
+++ Unit 03/Lesson 1/Coding Activity 1.java
@@ -0,0 +1,18 @@
+/* Lesson 1 Coding Activity Question 1 */
+
+import java.util.Scanner;
+
+public class U3_L1_Activity_One{
+  public static void main(String[] args){
+    
+    /* Write your code here */
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Please enter an integer:");
+    int x = scan.nextInt();
+    if (x == 176)
+    {
+      System.out.println("YES");
+    }
+  }
+}
