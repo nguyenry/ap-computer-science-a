34786a045c74077cedc5fb471f2e910438b2f694
nguyenry <80845748+nguyenry@users.noreply.github.com>
2021-05-23 12:24:55 -0700

    Create Coding Activity 2.java
    Write a program to test if a decimal value input from the keyboard is equal to 48.729. If it is, print "YES" (without the quotes).
    
    Sample run 1:
    Please enter a double:
    46.221
    
    Sample run 2:
    Please enter a double:
    48.729
    YES

---
+++ Unit 03/Lesson 1/Coding Activity 2.java
@@ -0,0 +1,18 @@
+/* Lesson 1 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+public class U3_L1_Activity_Two{
+  public static void main(String[] args){
+    
+    /* Write your code here */
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Please enter a double:");
+    double x = scan.nextDouble();
+    if (x == 48.729)
+    {
+      System.out.println("YES");
+    }
+  }
+}
